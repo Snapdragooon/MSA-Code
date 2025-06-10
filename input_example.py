003d90a9eee6b6ec8f9e1ad2b342873e9ec5314f
Snapdragooon <sophshea37@icloud.com>
2025-06-10 11:07:31 -0500

    Add files via upload

---
+++ input_example.py
@@ -0,0 +1,30 @@
+# write a pounds to kilograms converter program
+# a user will be prompted for their weight in pounds and the program will output weight in kgs
+
+# GET INPUT WEIGHT FROM THE USER
+# convert weight to a float
+# validate that weight is a number
+# if weight is not a number, reprompt the user until the input is correct
+def main():
+    while True:
+        try:
+            user_weight = float(input("Enter your weight in pounds: "))
+            # validate the user_weight is positive, if not positive, reprompt the user
+            if user_weight <= 0:
+                print("ERROR: Enter a value greater than 0")
+                continue
+            else:
+                break
+        except:
+            print("ERROR: Please enter a number")
+
+    # PROCESSING
+    # use a conversion to convert lbs to kilos: 2.205 lbs = 1kg
+    LBS_TO_KG_CONVERSION = 2.205
+    user_weight_in_kg = user_weight / LBS_TO_KG_CONVERSION
+
+    # OUTPUT
+    # print the output to the user in a nice format to 2 decimal places
+    print(f"You weight {user_weight_in_kg:.2f}kgs.")
+
+main()
