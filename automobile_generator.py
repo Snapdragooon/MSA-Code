4a775b97191302a23cd61ff9781dd369142b47e8
Snapdragooon <sophshea37@icloud.com>
2025-06-19 11:11:59 -0500

    Add files via upload

---
+++ automobile_generator.py
@@ -0,0 +1,24 @@
+from Automobile import Automobile
+
+def main():
+    # create instances of Automobile
+    auto1 = Automobile("Honda", "Accord", "23456", 2.2, "Alice", 2017, "Blue")
+    auto2 = Automobile("Ferrari", "F-50", "12345", 4.8, "Bob", 2022, "Black")
+
+    # change auto2 color
+    auto2.set_color("Red")
+
+    # change auto1 owner
+    auto1.set_owner("Enzo")
+
+    auto_list = []
+    auto_list.append(auto1)
+    auto_list.append(auto2)
+
+    # print each automobile's information
+    for auto in auto_list:
+        auto.print_data()
+
+    print(f"Auto 1 is {auto1.get_age()} years old")
+
+main()
